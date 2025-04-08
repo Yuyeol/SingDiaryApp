ed055d895b07714a31c5f212cb0911cb0e92503f
Yuyeol <uy23@naver.com>
2025-04-08 12:21:14 +0900

    init

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { colors } from "@/constants";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.ORANGE_600,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "홈",
+        }}
+      />
+      <Tabs.Screen
+        name="my"
+        options={{
+          title: "내 프로필",
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "설정",
+        }}
+      />
+    </Tabs>
+  );
+}
